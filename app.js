360bffb4afaab9dca718cc6392ac36bf2f0db492
Wojtek <ciubawojciech@gmail.com>
2020-02-27 20:39:46 +0100

    New exercise added

--- app.js
+++ app.js
@@ -107,3 +107,68 @@ document.writeln(
 
 // Optimized app with lots of stuff description:
 // We are fetching data from API, if there is more than one source, we can wrap few fetch functions in one Promise.all to be sure we will fetch all of them. Obviously we have to handle error responses. After that we should create separated functional component (in the future will be quicker than class-component) with image and stuff surrounding it. In main component we can display components with image using lazy load (react.lazy or react-lazyload) to load them only if user scrolls view. Detailed data inside image component could be fetched when image component is in use. To make performance better, we can load blurred very small image placeholder and after that, lazy load should make it normal optimized for web image. All image components could be wrapped by React fragment <> for not adding additional node to DOM. We could also use React.memo, then React virtual DOM won’t render unchanged nodes. All data should be stored at Redux state for better performance and accessibility.
+
+document.writeln(
+  "<br /><br /><br /><br />Napisz algorytm, który jeśli element macierzy o wymiarach M x N ma wartość zero ustawia cały wiersz i kolumnę tego elementu na 0. :<br />"
+);
+
+const input = [
+  [0, 2, 3, 4, 5, 6, 7, 8, 9],
+  [11, 12, 13, 14, 15, 16, 17, 18, 19],
+  [21, 22, 23, 24, 25, 26, 27, 28, 29],
+  [31, 32, 33, 0, 35, 36, 37, 38, 39],
+  [41, 42, 43, 0, 45, 0, 47, 48, 49],
+  [51, 52, 53, 54, 55, 56, 57, 58, 59],
+  [61, 62, 63, 64, 65, 66, 67, 68, 69]
+];
+
+function printMatrix(table) {
+  if (Array.isArray(table) == false) {
+    document.writeln(table + "<br />It is not an array!");
+  } else {
+    table.forEach(row => {
+      document.writeln("[");
+      row.forEach(cell => {
+        document.write(`${cell}, `);
+      });
+      document.writeln("]<br />");
+    });
+    document.writeln("<br />");
+  }
+}
+
+function clearRowsAndColumnsEqZero(input) {
+  const clearColumns = [];
+  const clearRows = [];
+  const rows = input.length;
+  const columns = input[0].length;
+
+  input.forEach((row, rowIndex) => {
+    row.forEach((cell, columnIndex) => {
+      if (cell === 0) {
+        clearColumns.push(columnIndex);
+        clearRows.push(rowIndex);
+      }
+    });
+  });
+
+  clearRows.forEach(row => {
+    for (let i = 0; i < columns; i++) {
+      input[row][i] = 0;
+    }
+  });
+
+  clearColumns.forEach(column => {
+    for (let i = 0; i < rows; i++) {
+      input[i][column] = 0;
+    }
+  });
+
+  return input;
+}
+
+document.write("Input:<br>");
+printMatrix(input);
+
+document.write("Output:<br>");
+printMatrix(clearRowsAndColumnsEqZero(input));
